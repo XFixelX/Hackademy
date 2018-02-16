d65be2cea6a151b9782c9ece9f533fe4689cef8e
Jakob Stammler <stammler.jakob@gmail.com>
2018-02-16 15:43:07 +0100

    Updated version of devisible function
    This version accounts for the fact that a could be bigger than b, which wouldn't work using the range function.

--- divisible.py
+++ divisible.py
@@ -1,5 +1,9 @@
 def divisible(a,b,c):
     new_list=[]
+    if a > b:
+        x = a
+        a = b
+        b = x
     for i in range(a,b+1):
         if i % c == 0:
             new_list.append(i)
