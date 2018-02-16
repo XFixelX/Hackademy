04e74d70b3773187ce307b2ff6245a75a49a2055
Unknown <Felixjohannes.rudolph@gmail.com>
2018-02-16 14:37:01 +0100

    Updated Test Code for square

--- test_square.py
+++ test_square.py
@@ -2,10 +2,10 @@ import unittest
 import production
 
 class TestProduction(unittest.TestCase):
-    def test_remove_duplicates(self):
-        input = [1,1,2,2]
-        output = [1,2]
-        self.assertEqual(output, production.remove_duplicates(input))
+    def test_square(self):
+        input = [2]
+        output = [4]
+        self.assertEqual(output, production.square(input))
 
 if __name__ =="__main__":
     unittest.main()
